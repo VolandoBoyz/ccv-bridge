b565004109e55fc4c3b9ea46c68ae659cbd85b18
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-01-16 18:33:39 +0100

    Modified indent rule on ESLint config to use spaces instead of tabs.

--- test/api.js
+++ test/api.js
@@ -1,20 +1,20 @@
 let chai = require("chai"),
-	chaiHttp = require("chai-http"),
-	chaiAsPromised = require("chai-as-promised"),
-	path = require("path"),
-	should = chai.should(),
-	app = require(path.resolve("src/app"));
+    chaiHttp = require("chai-http"),
+    chaiAsPromised = require("chai-as-promised"),
+    path = require("path"),
+    should = chai.should(),
+    app = require(path.resolve("src/app"));
 
 chai.use(chaiHttp);
 chai.use(chaiAsPromised);
 
 describe("api", () => {
-	// This test is just an example.
-	// Please delete it and write your own ones!
-	// (use sinon for mocking if you need it)
-	describe("/", () => {
-		it("should return 404 on no registered routes", () => {
-			chai.request(app).get("/").should.be.rejected;
-		});
-	});
+    // This test is just an example.
+    // Please delete it and write your own ones!
+    // (use sinon for mocking if you need it)
+    describe("/", () => {
+        it("should return 404 on no registered routes", () => {
+            chai.request(app).get("/").should.be.rejected;
+        });
+    });
 });
