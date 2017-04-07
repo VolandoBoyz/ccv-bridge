01f1283b886371d11971f0b05b5a800ff6745dd4
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-04-07 09:57:49 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    Added integration config.
    
    # This is the commit message #2:
    
    fix(Fixed project not running and including babel-env):

---
+++ config/integration.js
@@ -0,0 +1,3 @@
+module.exports = {
+  listeningPort: 3099
+}
--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-let config = require('config')
-let debug = require('debug')(process.env.npm_package_name + ':' + 'app')
-let app = require('express')()
-
-let start = listeningPort => {
-  let server = app.listen(listeningPort, () => {
-    debug('App running at http://localhost:' + server.address().port)
-  })
-  return server
-}
-
-module.exports = start(config.get('listeningPort'))
+// let config = require('config')
+// let debug = require('debug')(process.env.npm_package_name + ':' + 'app')
+// let app = require('express')()
+//
+// let start = listeningPort => {
+//   let server = app.listen(listeningPort, () => {
+//     debug('App running at http://localhost:' + server.address().port)
+//   })
+//   return server
+// }
+//
+// module.exports = start(config.get('listeningPort'))
